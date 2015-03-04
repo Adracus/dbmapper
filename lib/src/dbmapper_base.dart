33659f36a1860e62083238232fad7e3f4695c5af
Adracus <adracus@gmail.com>
2015-03-04 18:34:20 +0100

    Cleanup library

--- lib/src/dbmapper_base.dart
+++ lib/src/dbmapper_base.dart
@@ -1,9 +1,6 @@
-// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-// TODO: Put public facing types in this file.
-
 library dbmapper.base;
 
 export 'dbmapper_definition.dart';
 export 'dbmapper_memory.dart';
+export 'dbmapper_postgres.dart';
+export 'dbmapper_database.dart';
